a822677ba5a3ea7512458093075d98a85d4a650b
ethun <ethuntcg@gmail.com>
2024-10-26 13:18:04 +0600

    update

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ function audioPlay(){
      const audio= new Audio('bubble Pop.mp3');
      audio.play();
 }
-function audio(){
-     const audio= new Audio('Download.mp3');
+function audio() {
+     const audio = new Audio('Download.mp3');
      audio.play();
-}
+ 
+     setTimeout(() => {
+         window.location.href = "https://www.example.com";
+     }, 3000);
+ }
